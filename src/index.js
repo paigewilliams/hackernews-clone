8ae44bd0834d7655ff7304eaf4b686dcfa27286d
Paige Williams <paw145@humboldt.edu>
2019-04-06 22:20:51 -0700

    Add Link type definition; implement feed and Link in resolver.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
 const resolvers = {
   Query: {
-    info: () => `This is the API for my Hackersnews Clone`
+    info: () => `This is the API for my Hackersnews Clone!!`,
+    feed: () => links,
+    Link: {
+      id: (parent) => parent.id,
+      description: (parent) => parent.description,
+      url: (parent) => parent.url,
+    }
   }
 }
 
