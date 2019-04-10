e1fde7f5e1fab48584d788c8f7e61303de5b60d4
Paige Williams <paw145@humboldt.edu>
2019-04-09 20:41:21 -0700

    Add vote type and the ability to add a vote to a link from a user; Add vote subscription.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -4,11 +4,8 @@ const { APP_SECRET, getUserId } = require('../utils')
 
 async function signup(parent, args, context, info) {
   const password = await bcrypt.hash(args.password, 10)
-
   const user = await context.prisma.createUser({ ...args, password })
-
   const token = jwt.sign({ userId: user.id }, APP_SECRET)
-
   return {
     token,
     user,
@@ -17,23 +14,33 @@ async function signup(parent, args, context, info) {
 
 async function login(parent, args, context, info) {
   const user = await context.prisma.user({ email: args.email })
-
   if (!user) {
     throw new Error('No such user found!!')
   }
-
   const valid = await bcrypt.compare(args.password, user.password)
-
   if (!valid) {
     throw new Error('Invalid Password')
   }
-
   const token = jwt.sign({ userId: user.id }, APP_SECRET)
-
   return {
     token,
     user,
   }
+}
+
+async function vote(parent, args, context, info) {
+  const userId = getUserId(context)
+  const linkExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link: { id: args.linkId },
+  })
+  if (linkExists) {
+    throw new Error(`Already voted for link: ${args.linkId}`)
+  }
+  return context.prisma.createVote({
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } },
+  })
 }
 
 function post(parent, args, context, info) {
@@ -49,4 +56,5 @@ module.exports = {
   signup,
   login,
   post,
+  vote,
 }
